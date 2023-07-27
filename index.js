31567d09d88a10a0221d599ac282648e6ea49353
vivianthecoder <vivianlamyu@gmail.com>
2023-07-27 19:39:46 -0400

    Added API to GET List of all Inventory items

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors({origin: process.env.BACKEND_URL || 'http://localhost'}));
 app.use(express.json());
 app.use(express.static('public'))
 
-
+// GET List of all Inventory Items
+app.get("/inventory", (req, res) => {
+    knex
+        .select("*")
+        .from("inventories")
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).send("Error getting inventory");
+        })
+});
 
 app.listen(PORT, () => console.log("App is listening on port 8081"));
