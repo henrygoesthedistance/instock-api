7d5a7a955e4fdde3874b4663c40b033874668bc2
vivianthecoder <vivianlamyu@gmail.com>
2023-07-27 19:27:39 -0400

    Added nodemon, cors, and dotenv dependency packages

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
+const knex = require("knex")(require("./knexfile"));
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
 const PORT = process.env.PORT || 8081;
-
 
 app.use(cors({origin: process.env.BACKEND_URL || 'http://localhost'}));
 app.use(express.json());
 app.use(express.static('public'))
+
+
+
+app.listen(PORT, () => console.log("App is listening on port 8081"));
